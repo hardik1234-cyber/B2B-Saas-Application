c046a2bb71b4b98aa076850f2ce82f3449141ce9
HARDIK <hardikpandey10.hp@gmail.com>
2026-01-19 19:23:49 +0530

    auth system commit

---
+++ backend/app/core/auth.py
@@ -0,0 +1,9 @@
+import httpx
+from fastapi import Depends,HTTPException, status, Request
+from clerk_backend_api.security import AuthenticateRequestOptions
+from app.core.config import settings
+from app.core.clerk import clerk
+
+class AuthUser:
+    def __init__(self):
+        pass
